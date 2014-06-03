945a43b31de6734159941072f963b82ca77652d1
Alex Friedman <a.friedman07@gmail.com>
2014-06-03 00:02:53 -0400

    Initial Commit

---
+++ switfy-weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  switfy-weather
+//
+//  Created by Alex Friedman on 6/3/14.
+//  Copyright (c) 2014 Alex Friedman. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
